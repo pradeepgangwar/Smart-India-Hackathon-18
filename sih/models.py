149524faa38b5753221a3585f1817293547aafb9
arindam-modak <arindam.modak03@gmail.com>
2018-03-30 16:02:41 +0530

    models and views bugs removed

--- sih/models.py
+++ sih/models.py
@@ -105,5 +105,5 @@ class notifications(models.Model):
     message = models.CharField(max_length=300, null=False)
     is_read = models.BooleanField(default=False)
     def __str__(self):
-        return (self.user.username + " - " + self.dept.title)    
+        return (self.user.username + " - " + self.dept.title)
 
--- sih/views.py
+++ sih/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth import login, logout, authenticate
 from django.http import HttpResponse, HttpResponseRedirect
-from .forms import UserForm, UserProfileForm, Vacancy
+from .forms import UserForm, UserProfileForm, Vacancy,Applications,Query
 from django.contrib.auth.models import User
-from .models import UserProfile, vacancy, DeptProfile
+from .models import UserProfile, vacancy, DeptProfile, applications,query,notifications
 from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
@@ -58,58 +58,81 @@ def profile(request):
         (Female,  Female),
         (Others, Others),
     )
-    if request.method=="POST":
-        form = UserProfileForm(request.POST)
-        if form.is_valid():
-            print (1)
-            age = form.cleaned_data['age']
-            gender = request.POST.get('gender')
-            qualification = request.POST.get('qualification')
-            resume = request.FILES['resume']
-            profilepicture = request.FILES['profilepicture']
-            fs = FileSystemStorage()
-            filename_resume = fs.save(resume.name, resume)
-            uploaded_resume_url = fs.url(filename_resume)
+    if request.user.is_authenticated:
+        if request.method=="POST":
+            form = UserProfileForm(request.POST)
+            if form.is_valid():
+                print (1)
+                age = form.cleaned_data['age']
+                gender = request.POST.get('gender')
+                qualification = request.POST.get('qualification')
+                resume = request.FILES['resume']
+                profilepicture = request.FILES['profilepicture']
+                fs = FileSystemStorage()
+                filename_resume = fs.save(resume.name, resume)
+                uploaded_resume_url = fs.url(filename_resume)
 
-            filename_profilepicture = fs.save(profilepicture.name, profilepicture)
-            uploaded_profilepicture_url = fs.url(filename_profilepicture)
+                filename_profilepicture = fs.save(profilepicture.name, profilepicture)
+                uploaded_profilepicture_url = fs.url(filename_profilepicture)
 
-            if request.POST.get('is_subscribed')=="True":
-                is_subscribed = True
-            user = User.objects.get(username=request.user.username)
+                if request.POST.get('is_subscribed')=="True":
+                    is_subscribed = True
+                user = User.objects.get(username=request.user.username)
 
-            Profile = UserProfile.objects.create(user=user,age=age,gender=gender,resume=resume,profilepicture=profilepicture,is_subscribed=is_subscribed,qualification=qualification)
-            Profile.save()
+                Profile = UserProfile.objects.create(user=user,age=age,gender=gender,resume=resume,profilepicture=profilepicture,is_subscribed=is_subscribed,qualification=qualification)
+                Profile.save()
 
-            return render(request, 'sih/profile.html', {
-                'uploaded_resume_url': uploaded_resume_url,
-                'uploaded_profilepicture_url': uploaded_profilepicture_url,
-                'qualifications_choices':QUALIFICATIONS,
-                'age_choices':GENDER
-            })
+                return render(request, 'sih/profile.html', {
+                    'uploaded_resume_url': uploaded_resume_url,
+                    'uploaded_profilepicture_url': uploaded_profilepicture_url,
+                    'qualifications_choices':QUALIFICATIONS,
+                    'age_choices':GENDER
+                })
+
+        else:
+            form = UserProfileForm()
+
+        return render(request, 'sih/profile.html', {'form': form, 'status':'logged_in','qualifications_choices':QUALIFICATIONS,'age_choices':GENDER})
 
     else:
-        form = UserProfileForm()
-
-    return render(request, 'sih/profile.html', {'form': form, 'status':'logged_in','qualifications_choices':QUALIFICATIONS,'age_choices':GENDER})
+        return redirect('sih:signup')
 
 def vacancies(request):
+    if request.user.is_authenticated:
+        if request.method=="POST":
+            form = Vacancy(request.POST)
+            if form.is_valid():
+                title = request.POST.get('title')
+                description = request.POST.get('description')
+                num_slots = int(request.POST.get('num_slots'))
+                start_date = request.POST.get('start_date')
+                end_date = request.POST.get('end_date')
+                if request.POST.get('results_out')=="True":
+                    results_out = True
+                dept_user = DeptProfile.objects.get(user=request.user)
+                New_vacancy = vacancy.objects.create(title=title,description=description,num_slots=num_slots,start_date=start_date,end_date=end_date,results_out=results_out,dept_id=dept_user)
+                New_vacancy.save()
+        else:
+            form = Vacancy()
+            
+        return render(request, 'sih/vacancies.html', {'form': form, 'status':'logged_in'})
 
-    if request.method=="POST":
-        form = Vacancy(request.POST)
-        if form.is_valid():
-            title = request.POST.get('title')
-            description = request.POST.get('description')
-            num_slots = int(request.POST.get('num_slots'))
-            start_date = request.POST.get('start_date')
-            end_date = request.POST.get('end_date')
-            if request.POST.get('results_out')=="True":
-                results_out = True
-            dept_user = DeptProfile.objects.get(user=request.user)
-            New_vacancy = vacancy.objects.create(title=title,description=description,num_slots=num_slots,start_date=start_date,end_date=end_date,results_out=results_out,dept_id=dept_user)
-            New_vacancy.save()
     else:
-        form = Vacancy()
-        
-    return render(request, 'sih/vacancies.html', {'form': form, 'status':'logged_in'})
+        return redirect('sih:signup')
+
+def query(request):
+    if request.user.is_authenticated:
+        pass
+
+    else:
+        return redirect('sih:signup')
+
+def applications(request):
+    if request.user.is_authenticated:
+        pass
+
+    else:
+        return redirect('sih:signup')
+
+
 
