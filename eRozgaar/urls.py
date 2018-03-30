979543f3dadcebabcada20f3cc3961f7e9100d6f
shreyanshdwivedi <shreyanshdwivedi1997@gmail.com>
2018-03-30 11:04:15 +0530

    Authentication

--- eRozgaar/urls.py
+++ eRozgaar/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+from sih import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^accounts/login/$', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout'),
+    url(r'', include('sih.urls')),
 ]
--- sih/views.py
+++ sih/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
+def signup(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            new_user = User.objects.create_user(**form.cleaned_data)
+            login(new_user)
+            # redirect, or however you want to get to the main view
+            return HttpResponseRedirect('base.html')
+    else:
+        form = UserForm()
+
+    return render(request, 'signup.html', {'form': form, 'status':'logged_in'})
+
+def logout(request):
+    logout(request)
+
+    return HttpResponseRedirect('signup.html')
+
+def index(request):
+    return HttpResponseRedirect('base.html')
